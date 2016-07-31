d39f63a404af72c9f1a1afbbe9d7a3ff404c6a51
uhyo <uhyo@uhyohyo.net>
2016-07-31 18:47:36 +0900

    fix lifting

--- lib/lift.ts
+++ lib/lift.ts
@@ -4,6 +4,7 @@ import {
     Func,
     Exp,
     fv,
+    assign,
     make,
 } from './ast';
 import {
@@ -16,37 +17,36 @@ import {
 interface FuncDict{
     [name: string]: Func;
 }
-interface BindDict{
-    [name: string]: Array<Exp>;
+// FUncに対して追加の引数を渡す指示
+interface Binding{
+    name: string;
+    args: Array<Exp>;
 }
 
 export function lift({funcs, exp}: Program): Program{
+    // まずfree variablesの収集
     // 既存の関数のなまえ
     const glbs = Object.keys(funcs);
     const dns = glbs.concat(fv(exp, true));
     const fs: FuncDict = {};
-    const binds: BindDict = {};
     for(let name in funcs){
-        binds[name] = [];
+        fs[name] = lift_func(funcs[name], fs, glbs, dns);
     }
-    for(let name in funcs){
-        fs[name] = lift_func(funcs[name], fs, binds, glbs, dns);
-    }
-    const exp2 = lift_exp(exp, fs, binds, glbs, dns);
     return {
         funcs: fs,
-        exp: exp2,
+        exp: lift_exp(exp, fs, glbs, dns),
     };
 }
 
-function lift_func({args, body}: Func, fs: FuncDict, binds: BindDict, glbs: Array<string>, dns: Array<string>): Func{
+function lift_func({args, body}: Func, fs: FuncDict, glbs: Array<string>, dns: Array<string>): Func{
+    const body2 = lift_exp(body, fs, glbs, dns);
     return {
         args,
-        body: lift_exp(body, fs, binds, glbs, dns),
+        body: body2,
     };
 }
 
-function lift_exp(exp: Exp, fs: FuncDict, binds: BindDict, glbs: Array<string>, dns: Array<string>): Exp{
+function lift_exp(exp: Exp, fs: FuncDict, glbs: Array<string>, dns: Array<string>): Exp{
     switch(exp.type){
         case 'unit':
         case 'bconst':
@@ -55,39 +55,52 @@ function lift_exp(exp: Exp, fs: FuncDict, binds: BindDict, glbs: Array<string>, 
             return exp;
         case 'application': {
             const {exp1, args} = exp;
-            const exp1d = lift_exp(exp1, fs, binds, glbs, dns);
-            const argsd = args.map(e => lift_exp(e, fs, binds, glbs, dns));
-            if (exp1d.type === 'variable' && exp1d.name in binds){
-                // liftされた関数だこれ
-                return make.application(exp1d, argsd.concat(binds[exp1d.name]));
-            }
+            const exp1d = lift_exp(exp1, fs, glbs, dns);
+            const argsd = args.map(e => lift_exp(e, fs, glbs, dns));
             // ふつうだ
             return make.application(exp1d, argsd);
         }
         case 'branch': {
             const {cond, exp1, exp2} = exp;
 
-            const condd = lift_exp(cond, fs, binds, glbs, dns);
-            const exp1d = lift_exp(exp1, fs, binds, glbs, dns);
-            const exp2d = lift_exp(exp2, fs, binds, glbs, dns);
+            const condd = lift_exp(cond, fs, glbs, dns);
+            const exp1d = lift_exp(exp1, fs, glbs, dns);
+            const exp2d = lift_exp(exp2, fs, glbs, dns);
             return make.branch(condd, exp1d, exp2d);
         }
         case 'lambda': {
             const {args, body} = exp;
             // あたらしいおなまえ
             const fn = glbid('F');
-            // funcを生成
-            const body2 = lift_exp(body, fs, binds, glbs, dns);
-            // 自由変数を引数に突っ込む
-            const fvs = arrsub(fv(body2), glbs.concat(args));
-            const args2 = args.concat(fvs);
-            binds[fn] = fvs.map(n => make.variable(n));
 
-            const f = make.func(args2, body2);
+            // funcのbodyは先に変換しておく
+            const body2 = lift_exp(body, fs, glbs, dns);
+
+            // bodyに登場する自由変数を列挙する
+            const fvs = arrsub(fv(body2), glbs.concat(args));
+
+            // 変数名を付け替える
+            const fvs2 = fvs.map(x => glbid(x));
+            const asgn = fvs.map((old, i)=> ({
+                from: old,
+                to: make.variable(fvs2[i]),
+            }));
+            const body3 = assign(body2, asgn);
+            // 部分適用の関係で自由変数を先にする
+            const args2 = fvs2.concat(args);
+
+            // 新しい関数を入れる
+            const f = make.func(args2, body3);
+            // 関数を登録
             glbs.push(fn);
             dns.push(fn);
             fs[fn] = f;
-            return make.variable(fn);
+            // 関数に対する部分適用
+            if (fvs.length === 0){
+                return make.variable(fn);
+            }else{
+                return make.application(fn, fvs.map(x => make.variable(x)));
+            }
         }
     }
 }
